108eb063ea08bbbaad07b3eda21e705945fdef1e
Jose David Caro <j.caro0207@gmail.com>
2020-03-23 15:40:47 -0500

    [Fix style in task 0]

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Creates a file
 file { '/tmp/holberton':
-  path    => '/tmp/holberton',
   ensure  => file,
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
