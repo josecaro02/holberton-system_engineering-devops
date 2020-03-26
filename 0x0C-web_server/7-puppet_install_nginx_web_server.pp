df21e0eff8318c1600e458c61fd06703a7cbb41c
Jose David Caro <j.caro0207@gmail.com>
2020-03-25 21:44:05 -0500

    [Add task 7]

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,31 @@
 # Create a server and configure it
-$new_string = "\tserver_name _;\n\n\t location \/redirect_me {\n\treturn 301 youtube.com;\n}\n"
-$my_string = "server_name _;\n\terror_page 404 \/custom_404.html;\nlocation =
-	    \/custom_404.html {\nroot \/usr\/share\/nginx\/html;\ninternal;\n}\n"
-exec {
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'apt-get update -y'
+$new_string = "\tserver_name _;\n\n\t location /redirect_me {\n\treturn 301 youtube.com;\n}\n"
+
+exec { 'Update system':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'apt-get update -y',
   provider => 'shell',
 }
 
-exec {
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'apt-get install nginx -y'
+exec { 'Install nginx':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'apt-get install nginx -y',
   provider => 'shell',
 }
 
-exec {
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'echo "Holberton School" > /var/www/html/index.nginx-debian.html',
+exec { 'Set text in home page':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'echo "Holberton School" > /var/www/html/index.nginx-debian.html',
   provider => 'shell',
 }
-exec {
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
-
-exec {
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'echo "Ceci n'est pas une page" > /usr/share/nginx/html/custom_404.htmly',
+exec { 'Redirect page':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default',
   provider => 'shell',
 }
 
-exec {
-     path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-     command => 'sed -i \"s/server_name _;/$my_string',
-     provide => 'shell',
+exec { 'Restart web server':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'service nginx restart',
+  provider => 'shell',
 }
