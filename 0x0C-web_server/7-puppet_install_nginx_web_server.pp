18e12963134c02477255ceee898ca9a80227af04
Jose David Caro <j.caro0207@gmail.com>
2020-03-25 22:29:15 -0500

    [Add require statements]

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,34 @@
 # Create a server and configure it
 
-exec { 'Update system':
+exec { 'Update_system':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'sudo apt-get update -y',
   provider => 'shell',
   returns  => [100],
 }
 
-exec { 'Install nginx':
+exec { 'Install_nginx':
+  require  => Exec['Update_system'],
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'sudo apt-get install nginx -y',
   provider => 'shell',
 }
 
-exec { 'Set text in home page':
+exec { 'Set_text':
+  require  => Exec['Install_nginx'],
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
   provider => 'shell',
 }
-exec { 'Redirect page':
+exec { 'Redirect_page':
+  require  => Exec['Set_text'],
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'sudo sed -i "s/server_name _;/\tserver_name _;\n\n\t  location \/redirect_me {\n\treturn 301 youtube.com;\n}\n/" /etc/nginx/sites-available/default',
   provider => 'shell',
 }
 
-exec { 'Restart web server':
+exec { 'Restart_web_server':
+  require  => Exec['Redirect_page'],
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'sudo service nginx restart',
   provider => 'shell',
