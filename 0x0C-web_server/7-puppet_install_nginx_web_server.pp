6aa9b3502898e29ac9bdfb7436b193100f5bf265
Jose David Caro <j.caro0207@gmail.com>
2020-03-25 22:25:44 -0500

    [Add sudo to commands and change echo to tee]

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,31 +2,30 @@
 
 exec { 'Update system':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'apt-get update -y',
+  command  => 'sudo apt-get update -y',
   provider => 'shell',
   returns  => [100],
 }
 
 exec { 'Install nginx':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'apt-get install nginx -y',
+  command  => 'sudo apt-get install nginx -y',
   provider => 'shell',
 }
 
 exec { 'Set text in home page':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'echo "Holberton School" > /var/www/html/index.nginx-debian.html',
+  command  => 'echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
   provider => 'shell',
 }
 exec { 'Redirect page':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'sed -i "s/server_name _;/\tserver_name _;\n\n\t
-  location \/redirect_me {\n\treturn 301 youtube.com;\n}\n/" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "s/server_name _;/\tserver_name _;\n\n\t  location \/redirect_me {\n\treturn 301 youtube.com;\n}\n/" /etc/nginx/sites-available/default',
   provider => 'shell',
 }
 
 exec { 'Restart web server':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'service nginx restart',
+  command  => 'sudo service nginx restart',
   provider => 'shell',
 }
