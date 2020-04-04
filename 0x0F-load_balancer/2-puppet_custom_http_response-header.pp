97b6edddd25bfcea84f57c47910176fca095e69f
Jose David Caro <j.caro0207@gmail.com>
2020-04-03 21:41:36 -0500

    [Fix task 2 to make it only create the header]

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -15,21 +15,6 @@ exec { 'Install_nginx':
   returns  => [0,1],
 }
 
-exec { 'Set_text':
-  require  => Exec['Install_nginx'],
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
-  provider => 'shell',
-  returns  => [0,1],
-}
-exec { 'Redirect_page':
-  require  => Exec['Set_text'],
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'sudo sed -i "s/server_name _;/\tserver_name _;\n\n\t  location \/redirect_me {\n\treturn 301 youtube.com;\n}\n/" /etc/nginx/sites-available/default',
-  provider => 'shell',
-  returns  => [0,1],
-}
-
 exec { 'Add_header':
   require  => Eect['Redirect_page'],
   path     => ['/usr/bin', '/sbin', '/usr/sbin'],
