fd28bde6b301b79bdd8791376322ca3bc483f212
Jose David Caro <j.caro0207@gmail.com>
2020-05-04 18:21:03 -0500

    [Add task 3]

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script that uses a RESTAPI """
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    user_info = requests.get("https://jsonplaceholder.typicode.com/users")\
+                        .json()
+    list_user = {}
+    for user in user_info:
+        user_todos = requests.get(
+            "https://jsonplaceholder.typicode.com/users/" +
+            str(user.get('id')) + "/todos").json()
+        list_user[user.get('id')] = []
+        user_list = {}
+        for todo in user_todos:
+            user_list['username'] = user.get('username')
+            user_list['task'] = todo.get('title')
+            user_list['completed'] = todo.get('completed')
+            list_user[user.get('id')].append(user_list.copy())
+            user_list.clear()
+    with open('todo_all_employees.json', 'w') as outfile:
+        json.dump(list_user, outfile)
