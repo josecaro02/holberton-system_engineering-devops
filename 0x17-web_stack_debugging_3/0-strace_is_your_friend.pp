13ce180bbdf819b19efae3f45020033b3417a3ae
Jose David Caro <j.caro0207@gmail.com>
2020-05-20 22:24:59 -0500

    [Fix puppet]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 exec { 'Fix wp-settings':
-  require  => Exec['Set_text'],
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
   provider => 'shell',
