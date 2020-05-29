7b18332346c7ca0ea699419506d79bc0b7d56c8c
Jose David Caro <j.caro0207@gmail.com>
2020-05-28 20:06:19 -0500

    [Update task 0]

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,16 @@
 # Fix limit requests
-exec { 'Fix limit reqyest':
+exec { 'Fix_limit':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'sudo sed -i "s/UNLIMIT=.*/UNLIMIT=\"-n 200\"/"  /etc/default/nginx',
   provider => 'shell',
+  returns  => [0,1],
+}
+
+
+exec { 'Restart_nginx':
+  require  => Exec['Fix_limit'],
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'sudo service nginx restart,
+  provider => 'shell',
+  returns  => [0,1],
 }
